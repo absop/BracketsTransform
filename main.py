0dca9db4751c11ea11f72bd6ad47c07c61d80a5f
absop <2913049342@qq.com>
2019-09-06 14:59:56 +0800

    Update main.py.

--- main.py
+++ main.py
@@ -3,25 +3,22 @@ import sublime_plugin
 
 
 class BracketsOperater(sublime_plugin.TextCommand):
-    left_brackets  = ('(', '[', '{')
-    right_brackets = (')', ']', '}')
-    brackets = {
-        "(": ")",
-        "[": "]",
-        "{": "}",
-        ")": "(",
-        "]": "[",
-        "}": "{"
-    }
+    opening, closing = '([{', ')]}'
+    parents_string = "()[]{}"
+    parents = {}
+    for i in range(len(opening)):
+        parents[opening[i]] = closing[i]
+        parents[closing[i]] = opening[i]
+
     threshold = 1000000
 
     def cover(self, left, right):
         return sublime.Region(left.a, right.b)
 
-    def all_parents(self):
+    def select_cursor_parents(self):
         pairs = []
         for region in self.view.sel():
-            parents = self.parents(region.a, self.threshold)
+            parents = self.search_parents(region.a)
             if parents:
                 if pairs and parents == pairs[-1]:
                     continue
@@ -29,88 +26,77 @@ class BracketsOperater(sublime_plugin.TextCommand):
                     pairs.append(parents)
         return pairs
 
-    # 以后考虑渐进增大搜索区域，不然对于大文件的编辑，
-    # 即使光标置于空括号内部，也将会导致一定的延迟，鉴于人们一般不会对
-    # 不再视界之内的内容进行操作，故此处搜做的范围平均来说不会超过10000，
-    # 然而如下的一开始便对一个很大的范围进行 extract_tokens是很不合理的，
-    # 这是对大文件使用这些命令，会导致延迟的一个重要原因
-    def parents(self, point, threshold):
-        begin = max(point - threshold//2, 0)
-        end = min(begin + threshold, self.view.size())
-        extract_tokens = self.view.extract_tokens_with_scopes
-        ltokens = extract_tokens(sublime.Region(begin, point))
-        rtokens = extract_tokens(sublime.Region(point, end))
-        if ltokens and rtokens:
-            if ltokens[-1] == rtokens[0]:
-                ltokens.pop()
-            begin = (ltokens if ltokens else rtokens)[0][0].a
-            end = rtokens[-1][0].b
-            contents = self.view.substr(sublime.Region(begin, end))
-
-            stack = []
-            ltokens.reverse()
-            for region, scope in ltokens:
+    # TODO: Reduce delay
+    # A better scheme for searching parentheses,
+    # maybe increase the search region gradually
+    def search_parents(self, point):
+        def search(tokens, directed_parents):
+            for region, scope in tokens:
                 token = contents[region.a - begin:region.b - begin]
-                if token in self.brackets:
-                    # skip ignore
-                    if "comment" in scope or "string" in scope:
-                        continue
+                # skip ignore
+                if (token not in self.parents_string or
+                    "comment" in scope or "string" in scope or
+                    "char" in scope or "symbol" in scope):
+                    continue
 
-                    if token in self.right_brackets:
-                        stack.append(token)
-
-                    elif stack:
-                        if self.brackets[token] == stack[-1]:
-                            stack.pop()
+                if token in directed_parents:
+                    if stack:
+                        if self.parents[stack[-1]] == token:
+                            if match[1] is False:
+                                stack.pop()
+                            else:
+                                match[0] = region
+                                return True
                         else:
                             return False
                     else:
-                        left_region = region
-                        left_parent = token
+                        match[1] = region
+                        stack.append(token)
+                        return True
+                else:
+                    stack.append(token)
 
-                        for region, scope in rtokens:
-                            token = contents[region.a - begin:region.b - begin]
-                            if token in self.brackets:
-                                # skip ignore
-                                if "comment" in scope or "string" in scope:
-                                    continue
+            return False
 
-                                if token in self.left_brackets:
-                                    stack.append(token)
+        def get_toks(a, b):
+            return self.view.extract_tokens_with_scopes(sublime.Region(a, b))
 
-                                elif stack:
-                                    if self.brackets[token] == stack[-1]:
-                                        stack.pop()
-                                    else:
-                                        return False
+        begin = max(point - self.threshold//2, 0)
+        end = min(begin + self.threshold, self.view.size())
+        ltoks, rtoks = get_toks(begin, point), get_toks(point, end)
+        if not (ltoks and rtoks):
+            return False
+        if ltoks[-1] == rtoks[0]:
+            ltoks.pop()
 
-                                elif self.brackets[left_parent] == token:
-                                        return (left_region, region)
+        stack, match = [], [False, False]
+        begin, end = (ltoks if ltoks else rtoks)[0][0].a, rtoks[-1][0].b
+        contents = self.view.substr(sublime.Region(begin, end))
 
-                                else:
-                                    return False
-        return False
+        search(rtoks, self.closing) and search(reversed(ltoks), self.opening)
+
+        return match[0] and match
 
 
 class BracketsSelectorCommand(BracketsOperater):
     def run(self, edit):
-        for p in self.all_parents():
+        for p in self.select_cursor_parents():
             cover = self.cover(p[0], p[1])
             self.view.sel().add(cover)
 
 
 class BracketsTransformCommand(BracketsOperater):
     def run(self, edit, to):
-        for p in self.all_parents():
+        for p in self.select_cursor_parents():
             if self.view.substr(p[0]) == to:
                 continue
             self.view.replace(edit, p[0], to)
-            self.view.replace(edit, p[1], self.brackets[to])
+            self.view.replace(edit, p[1], self.parents[to])
 
 
 class BracketsTakeOffCommand(BracketsOperater):
     def run(self, edit):
-        parents = [p for p in self.all_parents()]
+        parents = [p for p in self.select_cursor_parents()]
         regions = [r for p in parents for r in p]
         selections = []
 
